0d74b701be197ca1eaf7ea037306fef5e9e8c58f
gguilhermelopes <guilhermelopes275@gmail.com>
2023-05-30 17:55:02 -0300

    feat: create loading page to reserve

---
+++ src/app/reserve/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="border-t text-gray-800">
+      <div className="py-9 px-5 w-full md:w-3/5 m-auto mt-5 animate-pulse bg-slate-200 h-[600px]"></div>
+    </div>
+  );
+};
+
+export default Loading;
