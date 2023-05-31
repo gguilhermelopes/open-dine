287996361ce1b28cafdb35010485842b463f52a6
gguilhermelopes <guilhermelopes275@gmail.com>
2023-05-30 22:04:04 -0300

    style: final adjustments

--- src/app/reserve/[slug]/loading.tsx
+++ src/app/reserve/[slug]/loading.tsx
@@ -1,7 +1,10 @@
+import SummaryForm from "./components/SummaryForm";
+
 const Loading = () => {
   return (
     <div className="border-t text-gray-800">
-      <div className="py-9 px-5 w-full md:w-3/5 m-auto mt-5 animate-pulse bg-slate-200 h-[600px]"></div>
+      <div className="py-9 px-5 w-[80%] md:w-3/5 m-auto mt-10 animate-pulse bg-slate-200 h-[100px]"></div>
+      <div className="py-9 px-5 w-[80%] md:w-3/5 m-auto mt-5 animate-pulse bg-slate-200 h-[300px]"></div>
     </div>
   );
 };
